30808a31ea1cf8b10ad4dd19cd404ff2738322a0
Eric Feng <erichfeng@gmail.com>
2017-07-11 23:17:57 -0700

    adding mode for render and hash

--- lib/jekyll-pushpress.rb
+++ lib/jekyll-pushpress.rb
@@ -1,49 +1,73 @@
+require 'base64'
 require 'digest'
 require 'json'
 
-def process_collection(collection, site, body)
-  body[collection.label] = []
+def process_collection(collection, site, mode)
+  result = []
 
   for doc in collection.docs
-    md5 = Digest::MD5.new
-    md5.update doc.content
-    doc.path.slice! site.source
-    body[collection.label].push({
+    doc.path.slice! (site.source + '/')
+    page_result = {
       'url': doc.url,
-      'hash': md5.hexdigest,
-      'date': doc.date,
-      'fields': doc.data,
       'path': doc.path,
-    })
+      'fields': doc.data,
+    }
+
+    if mode == 'hash'
+      md5 = Digest::MD5.new
+      md5.update doc.content
+      page_result['hash'] = md5.hexdigest
+    elsif mode == 'render'
+      page_result['content'] = Base64.encode64(doc.content)
+    end
+    result.push(page_result)
   end
-  return body
+
+  result
 end
 
 module JekyllPushPress
   Jekyll::Hooks.register :site, :pre_render do |site|
-    for doc in site.documents
-      doc.data['layout'] = 'pushpress'
+    if ENV['PUSHPRESS_MODE'] == 'hash'
+      site.time = Time.at(0)
+
+      for doc in site.documents
+        doc.data['layout'] = 'pushpress'
+      end
     end
   end
 
   Jekyll::Hooks.register :site, :post_render do |site|
+    mode = ENV['PUSHPRESS_MODE']
+
     body = {
-      :posts => [],
       :pages => [],
     }
 
     for page in site.pages
-      md5 = Digest::MD5.new
-      md5.update page.content
-      body[:pages].push({'url': page.url, 'hash': md5.hexdigest, 'fields': page.data})
+      page_result = {
+        'url': page.url,
+        'path': page.path,
+        'fields': page.data,
+      }
+
+      if mode == 'hash'
+        md5 = Digest::MD5.new
+        md5.update page.content
+        page_result['hash'] = md5.hexdigest
+      elsif mode == 'render'
+        page_result['content'] = Base64.encode64(page.content)
+      end
+      
+      body[:pages].push(page_result)
     end
 
     site.collections.each do |label, collection|
       if collection.label == 'posts'
-        body = process_collection(collection, site, body)
+        body[collection.label] = process_collection(collection, site, mode)
       else
         if collection.write?
-          body = process_collection(collection, site, body)
+          body[collection.label] = process_collection(collection, site, mode)
         end
       end
     end
